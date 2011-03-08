b44bb4416b5714262e07bcea0dcb5323b85600bd
zack <zack@dad95343-7cd6-4e6f-946e-cdd0e35697ce>
2011-03-08 10:34:21 +0000

    Adding and tagging my latest bug fixes (cross your fingers) as 1.0.1. This will be the last version before Dan's fixes, which should come tomorrow. (His version will be 1.0.2, if all goes well. 1.0.3 will be 1.0.2, but devoid of commented-out code plus some cleanups.)

---
+++ tags/1.0.1/pagecounter.php
@@ -0,0 +1,106 @@
+<?php
+
+include_once '../facebook/facebook.php';
+include_once 'common.php';
+
+ob_start();
+
+//session_start();
+//session_unset();
+
+
+//$fb = cmc_startup($appapikey, $appsecret,2);
+//$fbid = $fb->require_login($required_permissions = 'publish_stream');
+
+$con = arena_connect();
+
+// Originally I thought of using MYSQL to store "hits" counters, but somehow my CREATE SQL query
+// did not work. So using simple files to hold the total and unique hits to the welcome page - PB
+
+
+// This can be placed in index - CREATE hits table only if it does not exist - some logic needed there
+//mysql_query("CREATE TABLE hits (unique int(6), total int(7))");
+
+// Assumes: you are already connected to MySQL,
+// and have selected the database you will be using;
+// That the visitor is cookie-compatible;
+// That the visitor has not cleared their cookies.
+
+
+// Also note that this script is very optimizable.
+// It is intended only to work and to be easy to understand,
+// not to win any awards.
+
+$facebook = new Facebook($appapikey, $appsecret);
+$fbid = $facebook->require_login($required_permissions = 'publish_stream');
+
+//$uniquepage = "uniquehitcounter.txt";
+
+//$totalpage = "totalhits.txt";
+
+
+// The total number of hits is going to be incremented
+// regardless of the situation, so this can be done first
+// to get it out of the way
+
+  // Now, we set the cookie's value. Here, it is set to
+  // expire in 3600 seconds (1 hours) - you can
+  // change this is if you like, but it ensures that the
+  // same person won't be counted as a unique visitor
+  // more than once in a 1-hour period
+
+
+if(isset($_COOKIE["visited"])) {
+  $cookie = $_COOKIE["visited"];
+//  echo 'cookie value='.$cookie;
+  // Our global variable index will be called 'visited',
+  // to make it easy to remember
+} else {
+  $cookie = false;
+}
+
+//$unique_hits = file($uniquepage);
+//$unique_hits[0] = $unique_hits[0] + 0;
+
+//$ip = $_SERVER['REMOTE_ADDR'];
+
+if (!$cookie) {
+  
+  $sql = 'select * from hits where userid="'.$fbid.'"';
+  $result = mysql_query($sql);
+  $numrows = mysql_num_rows($result);
+  if ($numrows==0) {
+	$sql = 'insert into hits (userid,count) VALUES ("'.$fbid.'","1")';
+	$result = mysql_query($sql);
+  }
+  else {
+  $row = mysql_fetch_array($result);
+  $unique_hits = $row['count'] + 1;
+  $sql = 'update hits set count="'.$unique_hits.'" where userid="'.$fbid.'"';
+  $result = mysql_query($sql);
+  }
+
+  /*
+  $unique_hits[0]++;
+  $fp = fopen($uniquepage,"w");
+  fputs($fp,"$unique_hits[0]");
+  fclose($fp);
+  */
+
+  //$fd = fopen ($uniquepage , "w"); 
+  //$fcounted = $fstring."\n".getenv("REMOTE_ADDR");
+  //$fout= fwrite ($fd , $fcounted );
+  //fclose($fd); 
+
+  //mysql_query("UPDATE hits SET unique = '" . $unique_hits . "'");
+  // The value of $cookie is NOT true, meaning that the user
+  // has not yet been counted
+  //setcookie("visited","1",time()+3600);
+  setcookie("visited","1",time()+600);
+}
+
+//print "<b>PERFORMANCE STATISTICS:"."</b><br />";
+//print "<b>Total number of page hits=".$total_hits[0]."</b><br />";
+//print "<b>This application has been accessed ".$unique_hits[0]." times</b><br />";
+
+?>
---
+++ tags/1.0.1/searchbyzip.php
@@ -0,0 +1,155 @@
+<?php
+// Application: Christian Missions Connector
+// File: 'searchbyzip.php' 
+//  searches for users and/or trips based on zip code
+// 
+//require_once 'facebook.php';
+
+include_once 'common.php';
+ob_start();
+
+$fb = cmc_startup($appapikey, $appsecret,0);
+$fbid = $fb->require_login("publish_stream,read_stream");
+
+if (isset($_GET['error'])) {
+	$error = $_GET['error'];
+	if ($error==1)
+	echo '<br /><br /><b>All fields in this form are required fields, please enter them <b/><br /> <br />';
+	else if ($error==2)
+	echo '<br /><br /><b>A wrong Value parameter has been entered<b/><br /> <br />';
+}
+
+if (isset($_GET['value'])) {
+	$value = $_GET['value'];
+}
+	
+// get user information
+$info = $fb->api_client->users_getInfo($fbid, "name,email,current_location");
+
+for($i=0; $i < count($info); $i++) {
+  $record = $info[$i];
+  $name = $record['name'];
+  $email = $record['email'];
+  //$phone = $info[0]['phone'];
+  if ($value==1)
+	$current_country = "United States";
+  else
+    $current_country = $record['current_location']['country'];
+
+  $current_state = $record['current_location']['state'];
+  $current_city = $record['current_location']['city'];
+}
+
+?>
+<br/><br/>
+
+<fb:editor action="http://apps.facebook.com/missionsconnector/searchzipresults.php" method='get'>
+
+<?php //location
+
+if ((strstr($current_country,"United States")) || (strstr($current_country,"USA"))) {
+
+session_start();
+$_SESSION['locvalue'] = 1;
+// User selection of the zip code
+if (isset($_SESSION['zip'])) 
+echo '<fb:editor-text value="'.$_SESSION['zip'].'" label="User Zip Code (Required)" name="zip"/>';
+else
+echo '<fb:editor-text label="User Zip Code (Required)" name="zip"/>';
+
+//<fb:editor-text label="User Zip Code (Required)" name="zip"/>
+
+?>
+
+<?php
+// User selection of the search radius
+?>
+<fb:editor-custom label="Search Radius" name="searchradius">
+<select name="searchradius" id="searchradius" multiple="false">
+
+<?php
+if (isset($_SESSION['searchradius'])) {
+if ($_SESSION['searchradius']==5)
+echo '<option  selected="selected" value="5">5 Miles</option>';
+else
+echo '<option  value="5">5 Miles</option>';
+
+if ($_SESSION['searchradius']==10)
+echo '<option  selected="selected" value="10">10 Miles</option>';
+else
+echo '<option  value="10">10 Miles</option>';
+
+if ($_SESSION['searchradius']==25)
+echo '<option  selected="selected" value="25">25 Miles</option>';
+else
+echo '<option  value="25">25 Miles</option>';
+
+if ($_SESSION['searchradius']==50)
+echo '<option  selected="selected" value="50">50 Miles</option>';
+else
+echo '<option  value="50">50 Miles</option>';
+
+if ($_SESSION['searchradius']==100)
+echo '<option  selected="selected" value="100">100 Miles</option>';
+else
+echo '<option  value="100">100 Miles</option>';
+
+if ($_SESSION['searchradius']==500)
+echo '<option  selected="selected" value="500">500 Miles</option>';
+else
+echo '<option  value="500">500 Miles</option>';
+}
+else {
+echo '<option  value="5">5 Miles</option>';
+echo '<option  value="10">10 Miles</option>';
+echo '<option  value="25">25 Miles</option>';
+echo '<option  value="50">50 Miles</option>';
+echo '<option  value="100">100 Miles</option>';
+echo '<option  value="500">500 Miles</option>';
+}
+?>
+
+</select>
+</fb:editor-custom>
+
+<?php
+}
+// This means the user's country is not USA - this means the search criteria should be different
+else {
+
+echo '<br /><b> The facebook profile does not list your country as USA. If this is so, then enter the Country, State and City below <b /><br /><br />';
+echo "If your country is USA,<a href='http://apps.facebook.com/missionsconnector/searchbyzip.php?value=1'>click here</a><br /><br />";
+
+session_start();
+$_SESSION['locvalue'] = 2;
+
+?>
+
+<?php
+//<fb:editor action="http://apps.facebook.com/missionsconnector/searchzipresults.php?value=2" method='get'>;
+
+
+if (isset($_SESSION['lcountry'])) 
+echo '<fb:editor-text value="'.$_SESSION['lcountry'].'" label="User Country (Required)" name="country"/>';
+else
+echo '<fb:editor-text label="User Country (Required)" name="country"/>';
+
+if (isset($_SESSION['lstate'])) 
+echo '<fb:editor-text value="'.$_SESSION['lstate'].'" label="User State (Required)" name="state"/>';
+else
+echo '<fb:editor-text label="User State (Required)" name="state"/>';
+
+if (isset($_SESSION['lcity'])) 
+echo '<fb:editor-text value="'.$_SESSION['lcity'].'" label="User City (Required)" name="city"/>';
+else
+echo '<fb:editor-text label="User City (Required)" name="city"/>';
+
+}
+
+echo '<fb:editor-buttonset>';
+echo '<fb:editor-button value="Submit" name="submit"/>';
+echo '</fb:editor-buttonset>';
+
+?>
+
+</fb:editor>
