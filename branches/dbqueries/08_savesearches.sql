262a853d22a5f93142bc9ffed77a1238ecad5138
poornima <poornima@dad95343-7cd6-4e6f-946e-cdd0e35697ce>
2011-05-11 00:22:20 +0000

    Increased the size of the searchquery in searchterms table to varchar(2048) from varchar(512)

--- branches/dbqueries/08_savesearches.sql
+++ branches/dbqueries/08_savesearches.sql
@@ -1,2 +1,2 @@
 create table searches(searchid smallint(6) not null auto_increment, userid bigint(20) not null, FOREIGN KEY (userid) REFERENCES users(userid), PRIMARY KEY  (searchid)) type=InnoDB;
-create table searchterms(searchid smallint(6) not null, searchquery VARCHAR(512), FOREIGN KEY (searchid) REFERENCES searches(searchid)) type=InnoDB;
+create table searchterms(searchid smallint(6) not null, searchquery VARCHAR(2048), FOREIGN KEY (searchid) REFERENCES searches(searchid)) type=InnoDB;
