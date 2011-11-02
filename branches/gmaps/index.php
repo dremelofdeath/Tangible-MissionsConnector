37b3806d65fbbf7d76da7ee87573753a55036ce9
garrett <garrett@dad95343-7cd6-4e6f-946e-cdd0e35697ce>
2011-11-02 15:47:56 +0000

    Integrated the maps code with firstlight database. Both the trip making page and the front page map work properly now.

--- branches/gmaps/index.php
+++ branches/gmaps/index.php
@@ -5,9 +5,8 @@
 <body>
 <p>Gmaps Branch Index</p>
 <ul>
-   <li><a href="MapInterface.html">Trip making page</a> This page contains the necessary javascript to create a trip and save it to the database (but doesn't actually save anything yet)</li>
-   <li><a href="ReadLatLngstable.php">Front Page Map(broken)</a> This page loads sample trip data from my own personal server (for now of course) and uses it to populate a map with as many markers as are in the table</li>
+   <li><a href="MapInterface.html">Trip making page</a> This page allows you to create a location and save it to the firstlight database</li>
+   <li><a href="ReadLatLngstable.php">Front Page Map</a> This page loads sample trip data from our server and uses it to populate a map with as many markers as are in the table</li>
 </ul>
-<p>Note: The Front Page Map doesn't work yet because the free MySQL database I'm using doesn't allow remote access for free accounts. All the code is there, though, we just need to make space in OUR database for me to put the information in.</p>
 </body>
 </html>
