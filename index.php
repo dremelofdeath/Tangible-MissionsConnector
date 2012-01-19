0c36143cf43b389624912c3dd23649d5451f0d08
Zachary Murray <709873+dremelofdeath@users.noreply.github.com>
2012-01-19 13:03:46 +0000

    Tagging as 2.0 build 78.

--- index.php
+++ index.php
@@ -49,11 +49,11 @@ function cmc_js_load($src) {
 }
 
 function cmc_library_load_jquery($ver) {
-  cmc_js_load('http://code.jquery.com/jquery-'.$ver.'.min.js');
+  cmc_js_load('http://code.jquery.com/jquery-'.$ver.'.js');
 }
 
 function cmc_library_load_jquery_ui($ver) {
-  $src = 'https://ajax.googleapis.com/ajax/libs/jqueryui/'.$ver.'/jquery-ui.min.js';
+  $src = 'https://ajax.googleapis.com/ajax/libs/jqueryui/'.$ver.'/jquery-ui.js';
   cmc_js_load($src);
 }
 
@@ -229,7 +229,7 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
 
       .inner-tipbar-content li {
         margin: 15px;
-        line-height: 0px;
+        line-height: 1px; /* must not be zero due to a webkit bug */
       }
       
       h1.cmc-big-button-text {
@@ -462,6 +462,8 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
 	
     <link rel="stylesheet" type="text/css" href="jquery.multiselect.css" />
     <script type="text/javascript" src="jquery.multiselect.js"></script>
+	  
+  
   <script type="text/JavaScript">
   
  function tripzipdisplay() {
@@ -507,6 +509,8 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
         <li><a href="#profile-tab">Profile</a></li>
         <li><a href="#trips-tab">Trips</a></li>
         <li><a href="#search-tab">Search</a></li>
+        <li><a href="#network-tab">My Network</a></li>
+        <li><a href="#invite-tab">Invite</a></li>
       </ul>
       <div id="ajax-spinner">
         <img src="ajax-spinner.gif" />
@@ -574,6 +578,7 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
                 </div>
               </div>
               <div id="profile-right-content-section">
+                <span id="profile-website">www.example.com</span>
                 <div id="profile-section-skills">
                   <h3>Medical Skills:</h3>
                   <div id="profile-medskills">Medskills
@@ -712,7 +717,7 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
       </div>
       <div id="search-tab">
         <div id="search-box">
-          <div id="search-tipbar" style="display: none; position: relative; height: 16px">
+          <div id="search-tipbar" style="position: relative; height: 16px">
             <div id="search-tipbar-left" style="position: absolute; left: 2px;">
               <a class="tipbar-link" href="#">Search history...</a>
               <div class="tipbar-content" style="display: none">
@@ -837,9 +842,18 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
           </div>
         </div>
       </div>
+      <div id="network-tab">
+        <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+      </div>
+      <div id="invite-tab">
+        <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+        <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+      </div>
     </div>
     <div id="cmc-footer" style="display: none">
       <div class="leftside">
+        <a href="#" id="secret-hideout">Admin</a> :
+        <a href="#" id="report-problem">Report a Problem</a> : <a href="#" id="contact-link">Contact Us</a>
       </div>
       <div class="rightside">
         <a href="#" id="copyrights">Copyrights</a> :
@@ -1513,33 +1527,7 @@ function cmc_big_button($title, $subtext=FALSE, $onclick=FALSE, $img=FALSE, $img
           </div>
         </form>
       </div>
-      <!-- debug only dialogs! remove these before shipping!! -zack -->
-      <div id="assert-dialog" title="Assertion failure!">
-        <h2>You hit an assert!</h2>
-        <p id="assert-message"></p>
-      </div>
-      <div id="secret-hideout-dialog" title="Administration">
-        <p>This is an area for magical unicorns and rainbows.</p>
-      </div>
     </div>
-    <!-- The debug log. Should not be displayed by default. Enable via the admin panel. -->
-    <div id="debug-section" style="display: none">
-      <textarea id="debug-log" rows="10" cols="80" spellcheck="false">
-        Please wait, loading debug console...
-      </textarea>
-      <div id="debug-info-section">
-        <div id="requests-outstanding" style="display: inline; margin-right: 13px;">
-          requests outstanding: <span id="requests-outstanding-value">0</span>
-        </div>
-        <div id="logged-in-user" style="display: inline; margin-right: 13px;">
-          user: <span id="logged-in-user-value">(not in sync)</span>
-        </div>
-      <div id="debug-controls">
-        <button id="debug-detach-handlers">detach debug handlers</button>
-        <button id="debug-force-login">force login</button>
-      </div>
-    </div>
-    <!-- Do not place HTML markup below this line -->
   </body>
 </html>
 <!-- vim: ai:et:ts=2:sw=2
