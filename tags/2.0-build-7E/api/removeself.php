f148b3e0451b424812b3a442cabf3b391cc216c9
zack <zack@dad95343-7cd6-4e6f-946e-cdd0e35697ce>
2012-02-05 04:14:29 +0000

    This build (7E) fixes the following bugs:
    * Appcrash when the user deauthorizes and attempts to reauthorize the app
    * Create new profile UI vanishes to a blank page if the user cancels profile creation dialog
    * Blank UI when the user has a CMC profile, but hasn't authorized the app (i.e., removed it)
    * FB.login authorization is not requested before a new profile is created
    * New profile creation UI does not go away after creating a new profile
    * For long profiles, trip names do not scroll with the rest of the profile display
    * Build system: Partially synced or sparse checkouts generate strange buildnumbers
    * Build system: No way to check the current buildnumber
    * Long trip profiles render off the end of the page
    * The text of the trip profile is too close to the profile picture

---
+++ tags/2.0-build-7E/api/removeself.php
@@ -0,0 +1,111 @@
+<?php
+// Application: Christian Missions Connector
+// File: 'trips.php'
+//  shows all trips the user is a member of
+//
+//require_once 'facebook.php';
+
+include_once 'common.php';
+
+$con = arena_connect();
+
+$saferequest = cmc_safe_request_strip();
+$has_error = FALSE;
+$err_msg = '';
+
+if (array_key_exists('tripid', $saferequest) && array_key_exists('fbid', $saferequest)) {
+  // both tripid and facebook userid should be provided
+  $tripid = $saferequest['tripid'];
+  $fbid = $saferequest['fbid'];
+} 
+else {
+  // error case: neither required variable is defined
+  $has_error = TRUE;
+  $err_msg = "Required parameters not defined.";
+}
+
+$json = array();
+
+if (!$has_error) {
+
+// check the existence of the trip
+$sql = 'select * from trips where id="'.$tripid.'"';
+$result = mysql_query($sql,$con);
+if (!$result) {
+  setjsonmysqlerror($has_error,$err_msg,$sql);
+}
+else {
+
+ $numrows = mysql_num_rows($result);
+
+ if ($numrows == 0) {
+    $has_error = TRUE;
+    $err_msg = "No Trip exists with the specified ID";
+ }
+ else {
+if (isset($tripid)) {
+
+$sql = 'select * from tripmembers where tripid="'.$tripid.'" and userid="'.$fbid.'"';
+$result = mysql_query($sql,$con);
+if (!$result) {
+	setjsonmysqlerror($has_error,$err_msg,$sql);
+}
+else {
+	$numrows = mysql_num_rows($result);
+  if ($numrows==0) {
+		$has_error = TRUE;
+		$err_msg = "You are not a member of this trip";
+  }
+  else if ($numrows==1) {
+		$has_error = TRUE;
+		$err_msg = "You are the the only person on this trip, delete trip instead";
+	}
+	else {
+		$sql = 'delete from tripmembers where tripid="'.$tripid.'" and userid="'.$fbid.'"';
+		$result = mysql_query($sql,$con);
+		
+		if (!$result) {
+			setjsonmysqlerror($has_error,$err_msg,$sql);
+		}
+		else {
+
+		// Now decrement the number of people on this trip
+		$sql = 'select * from trips where id="'.$tripid.'"';
+		$result = mysql_query($sql,$con);
+		if (!$result) {
+			setjsonmysqlerror($has_error,$err_msg,$sql);
+		}
+		else {
+			$row = mysql_fetch_array($result);
+			$numpeople = $row['numpeople'] + 0;
+			$numpeople--;
+
+			$sql = 'update trips set numpeople="'.$numpeople.' where id="'.$tripid.'"';
+			$result = mysql_query($sql,$con);
+			if (!$result) {
+				setjsonmysqlerror($has_error,$err_msg,$sql);
+			}
+		}
+		}
+	}
+}
+
+}
+}
+
+}
+}
+
+$json['has_error'] = $has_error;
+
+if ($has_error) {
+  $json['err_msg'] = $err_msg;
+}
+
+echo json_encode($json);
+
+
+?>
+
+
+
