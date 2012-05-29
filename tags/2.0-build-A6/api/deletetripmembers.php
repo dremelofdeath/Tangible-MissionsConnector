1047741d307d8c14e0e34471bc043a7a3a217f8e
zack <zack@dad95343-7cd6-4e6f-946e-cdd0e35697ce>
2012-05-29 23:44:18 +0000

    setsail production upgrade: 2.0 Build A6

---
+++ tags/2.0-build-A6/api/deletetripmembers.php
@@ -0,0 +1,115 @@
+<?php
+// Application: Christian Missions Connector
+//
+
+include_once 'common.php';
+
+$con = arena_connect();
+
+$saferequest = cmc_safe_request_strip();
+$has_error = FALSE;
+$err_msg = '';
+
+if (array_key_exists('tripid', $saferequest) && array_key_exists('fbid', $saferequest)) {
+  // both tripid and facebook userid should be provided
+  $tid = $saferequest['tripid'];
+  $fbid = $saferequest['fbid'];
+} 
+else if (array_key_exists('Tripmembers', $saferequest) && array_key_exists('tripid', $saferequest)) {
+  // both tripid and facebook userid should be provided
+  $tid = $saferequest['tripid'];
+  $tripmembers = $saferequest['Tripmembers'];
+}
+else {
+  // error case: neither are defined
+  $has_error = TRUE;
+  $err_msg = "Required parameters was defined.";
+}
+
+$json = array();
+
+if (!$has_error) {
+
+  // check if there is a trip corresponding to $tid
+  $sql = 'select * from trips where id="'.$tid.'"';
+  $result = mysql_query($sql,$con);
+  if (!$result) {
+    setjsonmysqlerror($has_error,$err_msg,$sql);
+  } else {
+    $numrows = mysql_num_rows($result);
+    if ($numrows ==0) {
+      $has_error = TRUE;
+      $err_msg = "No Trip exists with the specified ID";
+    } else {
+      if (isset($fbid)) {
+        $sql = 'select * from tripmembers where userid="'.$fbid.'" and accepted="1" and tripid="'.$tid.'"';
+        $result = mysql_query($sql,$con);
+        if (!$result) {
+          setjsonmysqlerror($has_error,$err_msg,$sql);
+        } else {
+          $numrows = mysql_num_rows($result);
+          if ($numrows == 0) {
+            $has_error = TRUE;
+            $err_msg = 'User is not a member of that trip';
+          } else {
+            $sql = 'select COUNT(userid) from tripmembers where accepted="1" and tripid="'.$tid.'"';
+            $result = mysql_query($sql,$con);
+            if (!$result) {
+              setjsonmysqlerror($has_error,$err_msg,$sql);
+            } else {
+              $countrow = mysql_fetch_array($result);
+              $membercount = $countrow['COUNT(userid)'];
+              if ($membercount == 1) {
+                $has_error = TRUE;
+                $json['membercount'] = $membercount;
+                $err_msg = 'You are the only person in this trip'; 
+              } else {
+                $sql = 'DELETE FROM tripmembers WHERE userid="'.$fbid.'" AND tripid="'.$tid.'"';
+                $result = mysql_query($sql, $con);
+                if (!$result) {
+                  setjsonmysqlerror($has_error, $err_msg, $sql);
+                }
+              }
+            }
+          }
+        }
+      } else if (isset($tripmembers)) {
+
+        // Now we can delete members from the trip - which means updating the tripmembers table in the database
+
+        if (is_array($tripmembers)) {
+          while ($mytripmember = current($tripmembers)) {
+            $sql = 'delete from tripmembers where userid="'.$mytripmember.'" and tripid="'.$tid.'"';
+            $result = mysql_query($sql,$con);
+            if (!result) {
+              setjsonmysqlerror($has_error,$err_msg,$sql);
+              continue 1;
+            }
+            next($tripmembers);
+          }
+        } else {
+
+          $sql = 'delete from tripmembers where userid="'.$tripmembers.'" and tripid="'.$tid.'"';
+          $result = mysql_query($sql,$con);
+
+          if (!result) {
+            setjsonmysqlerror($has_error,$err_msg,$sql);
+          }
+        }
+      }
+    }
+  }
+}
+
+$json['has_error'] = $has_error;
+
+if ($has_error) {
+  $json['err_msg'] = $err_msg;
+}
+
+echo json_encode($json);
+
+?>
+
+
+
---
+++ tags/2.0-build-A6/api/searchtrips.php
@@ -0,0 +1,57 @@
+<?php
+include_once 'common.php';
+
+$con = arena_connect();
+
+$saferequest = cmc_safe_request_strip();
+$has_error = FALSE;
+$err_msg = '';
+
+$json = array();
+
+//first make sure that the user has a CMC profile, if not throw an error
+
+// get all trips that are in the future
+$sql = null;
+if (array_key_exists('fbid', $saferequest) && $saferequest['fbid'] != '') {
+  // first check that the user has a CMC profile - otherwise redirect user to create a profile
+  if (!db_check_user($saferequest['fbid'])) {
+    // This means user does not have a CMC profile
+    $has_error = TRUE;
+    $err_msg = "No CMC Profile";
+  } else {
+    if (array_key_exists('fbid', $saferequest) && $saferequest['fbid'] != '') {
+      $sql =
+        'SELECT t.*, tm.isadmin '.
+        'FROM trips AS t '.
+        'LEFT JOIN tripmembers AS tm '.
+        'ON t.id=tm.tripid AND tm.userid = "'.$saferequest['fbid'].'" '.
+        'WHERE t.departure >= NOW()';
+    } else {
+      $sql = 'SELECT * FROM trips WHERE departure >= NOW()';
+    }
+
+    $result = mysql_query($sql,$con);
+
+    if ($result) {
+      $numrows = mysql_num_rows($result);
+      $json['trips'] = array();
+
+      if ($numrows!=0) {
+        while ($row = mysql_fetch_array($result)) {
+          $json['trips'][] = $row;
+        }
+      }
+    } else {
+      setjsonmysqlerror($has_error,$err_msg,$sql);
+    }
+  }
+}
+
+$json['has_error'] = $has_error;
+
+if ($has_error) {
+  $json['err_msg'] = $err_msg;
+}
+
+echo json_encode($json);
